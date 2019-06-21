26a3d69ccc3cf0dd2d97a93e2f582a7535bad607
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-06-21 13:04:53 +0200

    added config files for pocketsphinx, doa and emotion_rec

--- systems/env/pepper_env.sh
+++ systems/env/pepper_env.sh
@@ -20,9 +20,14 @@ export PYTHONPATH=$PYTHONPATH:${prefix}/lib/python2.7/site_packages
 ##### config files
 
 export ORCHESTRATOR_CONFIG="${prefix}/share/esiaf_orchestrator/config/basic_orchestrator_config.yaml"
-export AUDIO_SEGMENTER_CONFIG="${prefix}/share/audio_segmenter/config/default.cfg"
+
 export GRABBER_CONFIG="${prefix}/share/esiaf_ros/config/basic_grabber_config.cfg"
 export PLAYER_CONFIG="${prefix}/share/esiaf_ros/config/basic_player_config.cfg"
 export RECORDER_CONFIG="${prefix}/share/esiaf_ros/config/basic_recorder_config.cfg"
 export SPLITTER_CONFIG="${prefix}/share/esiaf_channel_utils/config/basic_splitter_config.yaml"
 export WAV_PLAYER_CONFIG="${prefix}/share/esiaf_wav_player/config/basic_wav_config.yaml"
+
+export AUDIO_SEGMENTER_CONFIG="${prefix}/share/audio_segmenter/config/default.cfg"
+export POCKETSPHINX_CONFIG="${prefix}/share/esiaf_pocketsphinx/config/basic_ps_config.yaml"
+export DOA_CONFIG="${prefix}/share/esiaf_doa/config/basic_doa_config.yaml"
+export EMOTION_REC_CONFIG="${prefix}/share/esiaf_emotion_recognition/config/basic_emotion_rec_config.yaml"
