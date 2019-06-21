d1436a8fbd3d6fba8613e078ee648df255839fb4
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-06-21 12:43:23 +0200

    added several components: audio_segmenter, grabber, player, recorder, splitter and wav_player; as well as groups

--- systems/env/pepper_env.sh
+++ systems/env/pepper_env.sh
@@ -15,3 +15,14 @@ alias ros_env="source ${prefix}/setup.bash"
 
 export PATH="${prefix}/bin:$PATH"
 export PYTHONPATH=$PYTHONPATH:${prefix}/lib/python2.7/site_packages
+
+
+##### config files
+
+export ORCHESTRATOR_CONFIG="${prefix}/share/esiaf_orchestrator/config/basic_orchestrator_config.yaml"
+export AUDIO_SEGMENTER_CONFIG="${prefix}/share/audio_segmenter/config/default.cfg"
+export GRABBER_CONFIG="${prefix}/share/esiaf_ros/config/basic_grabber_config.cfg"
+export PLAYER_CONFIG="${prefix}/share/esiaf_ros/config/basic_player_config.cfg"
+export RECORDER_CONFIG="${prefix}/share/esiaf_ros/config/basic_recorder_config.cfg"
+export SPLITTER_CONFIG="${prefix}/share/esiaf_channel_utils/config/basic_splitter_config.yaml"
+export WAV_PLAYER_CONFIG="${prefix}/share/esiaf_wav_player/config/basic_wav_config.yaml"
