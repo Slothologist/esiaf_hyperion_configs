162149a58df9fd0f31e91210ac38f5bb3109b462
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-08-16 15:03:59 +0200

    added non-complete config for dataset eval pipeline 2; updated robocup task config

--- systems/env/pepper_env_robocup_task.sh
+++ systems/env/pepper_env_robocup_task.sh
@@ -19,8 +19,8 @@ export PYTHONPATH=$PYTHONPATH:${prefix}/lib/python2.7/site_packages
 export ORCHESTRATOR_CONFIG="${prefix}/share/esiaf_orchestrator/config/eval_robocup_task_config.yaml"
 
 export GRABBER_CONFIG="${prefix}/share/esiaf_ros/config/pepper_robot_grabber.cfg"
-export SPLITTER_CONFIG="${prefix}/share/esiaf_channel_utils/config/pepper_robot_splitter_config.yaml"
+export SPLITTER_CPP_CONFIG="${prefix}/share/esiaf_ros/config/pepper_robot_splitter.cfg"
 
 export AUDIO_SEGMENTER_CONFIG="${prefix}/share/audio_segmenter/config/pepper_robot.cfg"
-export POCKETSPHINX_CONFIG="${prefix}/share/esiaf_pocketsphinx/config/basic_ps_config.yaml"
+export POCKETSPHINX_CONFIG="${prefix}/share/esiaf_pocketsphinx/config/robocup_task.yaml"
 export DOA_CONFIG="${prefix}/share/esiaf_doa/config/pepper_robot_config.yaml"
