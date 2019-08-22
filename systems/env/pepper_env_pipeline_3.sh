ac5ec0074a5d316f9d48424658b1f378b97ff367
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-08-22 15:44:21 +0200

    added config for pipeline 3

---
+++ systems/env/pepper_env_pipeline_3.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+export prefix="/vol/systems/speech-pipeline-melodic/"
+
+if [ -z "$SIMMODE" ]; then
+    export pepper=`hostname -s`
+else
+    export pepper='pepper-robot'
+fi
+
+export robot=$pepper
+export machine=$robot
+
+export ROS_MASTER_URI=http://$robot:11311
+
+alias ros_env="source ${prefix}/setup.bash"
+
+export PATH="${prefix}/bin:$PATH"
+export PYTHONPATH=$PYTHONPATH:${prefix}/lib/python2.7/site_packages
+
+
+##### config files
+
+export ORCHESTRATOR_CONFIG="${prefix}/share/esiaf_orchestrator/config/eval_pipeline_3.yaml"
+
+export WAV_PLAYER_CONFIG="${prefix}/share/esiaf_wav_player/config/eval_pipeline_2.yaml"
+export AUDIO_SEGMENTER_CONFIG="${prefix}/share/audio_segmenter/config/eval_pipeline_2.cfg"
+export POCKETSPHINX_CONFIG="${prefix}/share/esiaf_pocketsphinx/config/eval_pipeline_2.yaml"
